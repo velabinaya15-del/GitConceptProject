8359832bb02fc3d83020a9096ebf688309a6bd40
abinaya <velabinaya15@gmail.com>
2026-02-17 16:20:36 +0530

    conflict sucessfully resolved

--- src/test/java/org/git/GitConcept.java
+++ src/test/java/org/git/GitConcept.java
@@ -3,13 +3,9 @@ package org.git;
 public class GitConcept {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
-		System.out.println("All is Well");
-=======
-		
 		System.out.println("Everything Happens For A Reason");
-		
->>>>>>> d7ce87f98440a1a84c6faa007d95f8307f71e9ab
+		System.out.println("All is Well");
+
 	}
 }
